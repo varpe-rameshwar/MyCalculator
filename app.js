6787ad5e65ed51c10dd214b16b305f019a69e9ea
varpe-rameshwar <158450975+varpe-rameshwar@users.noreply.github.com>
2024-02-07 14:58:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
             case 'AC':
                 clearDisplay();
                 break;
+            case '%':
             case '/':
             case '*':
             case '-':
@@ -24,6 +25,9 @@ document.addEventListener("DOMContentLoaded", () => {
                 break;
             case '=':
                 handleResult();
+                break;
+            case '⌫':
+                handleDelete();
                 break;
             default:
                 handleClickedDigit(val);
@@ -39,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
         display.value += val;
     }
 
-    const operators = ['/', '*', '+', '-'];
+    const operators = ['%','/', '*', '+', '-'];
     function handleOperators(val) {
         let displayLength = display.value.length
         let lastChar = display.value.charAt(displayLength - 1);
@@ -67,54 +71,82 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    function handleDelete(){
+        let displayArr = display.value.split('');
+        displayArr.pop();
+        display.value = displayArr.join('');
+    }
+
+    // function getLeftOperand(resultString,j){
+    //     let leftNumber = '';
+    //     let i = j-1;
+    //     while(!operators.includes(resultString.charAt(i)) && i >= 0){
+    //         leftNumber = resultString.charAt(i) + leftNumber;
+    //         i--;
+    //     }
+    //     return Number(leftNumber);
+    // }
+
+    // function getRightOperand(resultString,j){
+    //     let rightNumber = '';
+    //     let i = j + 1;
+    //     while(!operators.includes(resultString.charAt(i)) && i < resultString.length){
+    //         rightNumber += resultString.charAt(i);
+    //         i++;
+    //     }
+    //     return Number(rightNumber);
+    // }
+
+    // function calculateExpression(resultString, j) {
+    //     let leftOperand = getLeftOperand(resultString, j);
+    //     let rightOperand = getRightOperand(resultString, j);
+    //     switch (resultString.charAt(j)) {
+    //         case '/':
+    //             return leftOperand / rightOperand;
+    //         case '*':
+    //             return leftOperand * rightOperand;
+    //         case '-':
+    //             return leftOperand - rightOperand;
+    //         case '+':
+    //             return leftOperand + rightOperand;
+    //         case '%':
+    //             return leftOperand % rightOperand;
+    //     }
+    // }
+
+    // function getReplacableString(resultString,j){
+    //     let replacableString = '';
+    //     for(let i = j - 1; !operators.includes(resultString.charAt(i)) && i >= 0; i--){
+    //         // begin = i;
+    //         replacableString = resultString.charAt(i) + replacableString;
+    //     }
+    //     for(let k = j; !operators.includes(resultString.charAt(k)) && k < resultString.length; k++){
+    //         // end = k;
+    //         replacableString += resultString.charAt(k);
+    //     }
+    //     return replacableString;
+    // }
+
     const handleResult = () => {
-        let operatorPrecedency = ['%','/','*','-','+'];
-        // display.value = eval(display.value); 123*34/456+657-23
-        let resultString = display.value;
-        let result;
-        for(let i = 0; i < operatorPrecedency.length; i++){
-            for(let j = 0; j < resultString.length;j++){
-                if(operatorPrecedency[i] == resultString.charAt(j)){
-                    result = calculateExpression(resultString,j);
-                }
-            }
-        }
-    }
+    //     let operatorPrecedency = ['%','/','*','-','+'];
+        display.value = eval(display.value); 123*34/2+657-23
+        // let resultString = display.value;
+        // let newResult = '';
+        // let result;
+        // for(let i = 0; i < operatorPrecedency.length; i++){
+        //     for(let j = 0; j < resultString.length;j++){
+        //         if(operatorPrecedency[i] == resultString.charAt(j)){
+        //             result = calculateExpression(resultString,j);//result = 17
+        //             // resultString = replacableString(resultString,j);
+        //             // newResult = resultString.replace(getReplacableString(resultString,j),result);
+        //             let replacableString = getReplacableString(resultString,j);
+        //             resultString = resultString.substring(0,j-replacableString.length) +
+        //                             result +
+        //                             resultString.substring(replacableString.length);
 
-    function calculateExpression(resultString, j) {
-        let leftOperand = leftOperand(resultString, j);
-        let rightOperand = rightOperand(resultString, j);
-        switch (resultString.charAt(j)) {
-            case '/':
-                return leftOperand / rightOperand;
-            case '*':
-                return leftOperand * rightOperand;
-            case '-':
-                return leftOperand - rightOperand;
-            case '+':
-                return leftOperand + rightOperand;
-            case '%':
-                return leftOperand % rightOperand;
-        }
-    }
-
-    function leftOperand(resultString,j){
-        let leftNumber = '';
-        let i = j-1;
-        while(!operators.includes(resultString.charAt(i)) && i >= 0){
-            leftNumber = resultString.charAt(i) + leftNumber;
-            i--;
-        }
-        return Number(leftNumber);
-    }
-
-    function rightOperand(resultString,j){
-        let rightNumber = '';
-        let i = j + 1;
-        while(!operators.includes(resultString.charAt(i)) && i < resultString.length){
-            rightNumber += resultString.charAt(i);
-            i++;
-        }
-        return Number(rightNumber);
+        //         }
+        //     }
+        // }
+        // display.value = resultString;
     }
 })
